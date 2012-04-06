f0f99e7a5dad1ad3970cfc4081f308130ad0be03
MihaVucko <miha_vucko@hotmail.com>
2012-04-06 12:57:33 +0200

    naloga za 1.teden

---
+++ Projekt_praktikum/Projekt_praktikum/Form1.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Projekt_praktikum
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+            ZaporedjeTonov mojeZaporedje = new ZaporedjeTonov();
+            mojeZaporedje.dodajNoto("C", 0, 1);
+            mojeZaporedje.dodajNoto("D", 1, 1);
+            mojeZaporedje.dodajNoto("B", 7, 1);
+            mojeZaporedje.dodajPavzo(1);
+            MessageBox.Show(mojeZaporedje.porocilo());
+        }
+    }
+}
