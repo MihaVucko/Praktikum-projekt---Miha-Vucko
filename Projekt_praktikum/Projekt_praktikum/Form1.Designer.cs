661858a3e2bbeed0d7f59181713913e2a51aa3e6
MihaVucko <miha_vucko@hotmail.com>
2012-04-20 01:50:53 +0200

    naloga za 3 teden.

--- Projekt_praktikum/Projekt_praktikum/Form1.Designer.cs
+++ Projekt_praktikum/Projekt_praktikum/Form1.Designer.cs
@@ -28,20 +28,189 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.imageBox1 = new Emgu.CV.UI.ImageBox();
+            this.comboKljuc = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.groupBox4 = new System.Windows.Forms.GroupBox();
+            this.radioZvisana = new System.Windows.Forms.RadioButton();
+            this.radioNavadna = new System.Windows.Forms.RadioButton();
+            this.comboTempo = new System.Windows.Forms.ComboBox();
+            this.comboTaktStevec = new System.Windows.Forms.ComboBox();
+            this.comboTaktImenovalec = new System.Windows.Forms.ComboBox();
+            this.labelInfo = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.imageBox1)).BeginInit();
+            this.groupBox4.SuspendLayout();
             this.SuspendLayout();
+            // 
+            // imageBox1
+            // 
+            this.imageBox1.FunctionalMode = Emgu.CV.UI.ImageBox.FunctionalModeOption.Minimum;
+            this.imageBox1.Location = new System.Drawing.Point(12, 12);
+            this.imageBox1.Name = "imageBox1";
+            this.imageBox1.Size = new System.Drawing.Size(700, 500);
+            this.imageBox1.TabIndex = 2;
+            this.imageBox1.TabStop = false;
+            this.imageBox1.MouseClick += new System.Windows.Forms.MouseEventHandler(this.imageBox1_MouseClick);
+            // 
+            // comboKljuc
+            // 
+            this.comboKljuc.FormattingEnabled = true;
+            this.comboKljuc.Items.AddRange(new object[] {
+            "Violinski",
+            "Basovski"});
+            this.comboKljuc.Location = new System.Drawing.Point(718, 98);
+            this.comboKljuc.Name = "comboKljuc";
+            this.comboKljuc.Size = new System.Drawing.Size(121, 21);
+            this.comboKljuc.TabIndex = 5;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(721, 82);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(33, 13);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Ključ:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(721, 134);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(43, 13);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "Tempo:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(721, 187);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(32, 13);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "Takt:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(752, 206);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(12, 13);
+            this.label4.TabIndex = 11;
+            this.label4.Text = "/";
+            // 
+            // groupBox4
+            // 
+            this.groupBox4.Controls.Add(this.radioZvisana);
+            this.groupBox4.Controls.Add(this.radioNavadna);
+            this.groupBox4.Location = new System.Drawing.Point(718, 12);
+            this.groupBox4.Name = "groupBox4";
+            this.groupBox4.Size = new System.Drawing.Size(153, 67);
+            this.groupBox4.TabIndex = 14;
+            this.groupBox4.TabStop = false;
+            this.groupBox4.Text = "Nota";
+            // 
+            // radioZvisana
+            // 
+            this.radioZvisana.AutoSize = true;
+            this.radioZvisana.Location = new System.Drawing.Point(6, 42);
+            this.radioZvisana.Name = "radioZvisana";
+            this.radioZvisana.Size = new System.Drawing.Size(63, 17);
+            this.radioZvisana.TabIndex = 7;
+            this.radioZvisana.TabStop = true;
+            this.radioZvisana.Text = "Zvišana";
+            this.radioZvisana.UseVisualStyleBackColor = true;
+            // 
+            // radioNavadna
+            // 
+            this.radioNavadna.AutoSize = true;
+            this.radioNavadna.Location = new System.Drawing.Point(6, 19);
+            this.radioNavadna.Name = "radioNavadna";
+            this.radioNavadna.Size = new System.Drawing.Size(69, 17);
+            this.radioNavadna.TabIndex = 6;
+            this.radioNavadna.TabStop = true;
+            this.radioNavadna.Text = "Navadna";
+            this.radioNavadna.UseVisualStyleBackColor = true;
+            // 
+            // comboTempo
+            // 
+            this.comboTempo.FormattingEnabled = true;
+            this.comboTempo.Location = new System.Drawing.Point(718, 150);
+            this.comboTempo.Name = "comboTempo";
+            this.comboTempo.Size = new System.Drawing.Size(121, 21);
+            this.comboTempo.TabIndex = 15;
+            this.comboTempo.SelectedIndexChanged += new System.EventHandler(this.comboTempo_SelectedIndexChanged);
+            // 
+            // comboTaktStevec
+            // 
+            this.comboTaktStevec.FormattingEnabled = true;
+            this.comboTaktStevec.Location = new System.Drawing.Point(718, 203);
+            this.comboTaktStevec.Name = "comboTaktStevec";
+            this.comboTaktStevec.Size = new System.Drawing.Size(35, 21);
+            this.comboTaktStevec.TabIndex = 16;
+            this.comboTaktStevec.SelectedIndexChanged += new System.EventHandler(this.comboTaktStevec_SelectedIndexChanged);
+            // 
+            // comboTaktImenovalec
+            // 
+            this.comboTaktImenovalec.FormattingEnabled = true;
+            this.comboTaktImenovalec.Location = new System.Drawing.Point(761, 203);
+            this.comboTaktImenovalec.Name = "comboTaktImenovalec";
+            this.comboTaktImenovalec.Size = new System.Drawing.Size(35, 21);
+            this.comboTaktImenovalec.TabIndex = 17;
+            this.comboTaktImenovalec.SelectedIndexChanged += new System.EventHandler(this.comboTaktImenovalec_SelectedIndexChanged);
+            // 
+            // labelInfo
+            // 
+            this.labelInfo.AutoSize = true;
+            this.labelInfo.Location = new System.Drawing.Point(715, 248);
+            this.labelInfo.Name = "labelInfo";
+            this.labelInfo.Size = new System.Drawing.Size(0, 13);
+            this.labelInfo.TabIndex = 18;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(595, 426);
+            this.ClientSize = new System.Drawing.Size(883, 521);
+            this.Controls.Add(this.labelInfo);
+            this.Controls.Add(this.comboTaktImenovalec);
+            this.Controls.Add(this.comboTaktStevec);
+            this.Controls.Add(this.comboTempo);
+            this.Controls.Add(this.groupBox4);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.comboKljuc);
+            this.Controls.Add(this.imageBox1);
             this.Name = "Form1";
             this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.imageBox1)).EndInit();
+            this.groupBox4.ResumeLayout(false);
+            this.groupBox4.PerformLayout();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private Emgu.CV.UI.ImageBox imageBox1;
+        private System.Windows.Forms.ComboBox comboKljuc;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.GroupBox groupBox4;
+        private System.Windows.Forms.RadioButton radioZvisana;
+        private System.Windows.Forms.RadioButton radioNavadna;
+        private System.Windows.Forms.ComboBox comboTempo;
+        private System.Windows.Forms.ComboBox comboTaktStevec;
+        private System.Windows.Forms.ComboBox comboTaktImenovalec;
+        private System.Windows.Forms.Label labelInfo;
     }
 }
 
--- Projekt_praktikum/Projekt_praktikum/ZaporedjeTonov.cs
+++ Projekt_praktikum/Projekt_praktikum/ZaporedjeTonov.cs
@@ -11,19 +11,15 @@ namespace Projekt_praktikum
         private List<Znak> zaporedje;
         private int tempo;
         private double takt;
+        int stevec, imenovalec;
 
         public ZaporedjeTonov()
         {
             this.zaporedje = new List<Znak>();
             this.tempo = 120;
             this.takt = 1;
-        }
-
-        public ZaporedjeTonov(int tempo, double takt)
-        {
-            this.zaporedje = new List<Znak>();
-            this.tempo = tempo;
-            this.takt = takt;
+            this.stevec = 4;
+            this.imenovalec = 4;
         }
 
         public void dodajNoto(string ton, int oktava, double trajanje)
@@ -36,9 +32,9 @@ namespace Projekt_praktikum
             this.zaporedje.Add(new Znak(trajanje));
         }
 
-        public void izbrisiNoto(int index)
+        public void izbrisi(int index)
         {
-            if (index < this.zaporedje.Count && this.zaporedje[index].getFrekvenca() != 0)
+            if (index < this.zaporedje.Count)
             {
                 this.zaporedje.RemoveAt(index);
             }
@@ -48,66 +44,39 @@ namespace Projekt_praktikum
             }
         }
 
-        public void izbrisiPavzo(int index)
+        public List<Znak> getZaporedje()
         {
-            if (index < this.zaporedje.Count && this.zaporedje[index].getFrekvenca() == 0)
-            {
-                this.zaporedje.RemoveAt(index);
-            }
-            else
-            {
-                MessageBox.Show("Napaka!");
-            }
+            return this.zaporedje;
         }
 
-        public string porocilo()
+        public void setTempo(int tempo)
         {
-            string porocilo = "";
-            foreach (Znak trenutna in this.zaporedje)
-            {
-                porocilo += trenutna.getFrekvenca().ToString() + "   ";
-            }
+            this.tempo = tempo;
+        }
+
+        public int getTempo()
+        {
+            return this.tempo;
+        }
+
+        public void setTakt(int stevec, int imenovalec)
+        {
+            this.stevec = stevec;
+            this.imenovalec = imenovalec;
+            this.takt = Convert.ToDouble(stevec) / Convert.ToDouble(imenovalec);
+        }
+
+        public double getTakt()
+        {
+            return this.takt;
+        }
+
+        public string getPorocilo()
+        {
+            string porocilo = "Ključ: " + "\n";
+            porocilo += "Tempo: " + this.tempo.ToString() + " bpm" + "\n";
+            porocilo += "Takt: " + this.stevec.ToString() + " / " + this.imenovalec;
             return porocilo;
-        }
-
-        private class Znak
-        {
-            private double[,] frekvence = new double[,]{{16.35,	17.32,	18.35,	19.45,	20.60,	21.83,	23.12,	24.50,	25.96,	27.50,	29.14,	30.87},
-                                                    {32.70,	34.65,	36.71,	38.89,	41.20,	43.65,	46.25,	49.00,	51.91,	55.00,	58.27,	61.74},
-                                                    {65.41,	69.30,	73.42,	77.78,	82.41,	87.31,	92.50,	98.00,	103.8,	110.0,	116.5,	123.5},
-                                                    {130.8,	138.6,	146.8,	155.6,	164.8,	174.6,	185.0,	196.0,	207.7,	220.0,	233.1,	246.9},
-                                                    {261.6,	277.2,	293.7,	311.1,	329.6,	349.2,	370.0,	392.0,	415.3,	440.0,	466.2,	493.9},
-                                                    {523.3,	554.4,	587.3,	622.3,	659.3,	698.5,	740.0,	784.0,	830.6,	880.0,	932.3,	987.8},
-                                                    {1047,	1109,	1175,	1245,	1319,	1397,	1480,	1568,	1661,	1760,	1865,	1976},
-                                                    {2093,	2217,	2349,	2489,	2637,	2794,	2960,	3136,	3322,	3520,	3729,	3951},
-                                                    {4186,	4435,	4699,	4978,	5274,	5588,	5920,	6272,	6645,	7040,	7459,	7902}};
-
-            private string[] toni = new string[] { "C", "C#", "D", "Eb", "E", "F", "F#", "G", "G#", "A", "Bb", "B" };
-
-            private double trajanje;
-            private double frekvenca;
-
-            public Znak(double trajanje)
-            {
-                this.trajanje = trajanje;
-                this.frekvenca = 0;
-            }
-
-            public Znak(double trajanje, string ton, int oktava)
-            {
-                this.trajanje = trajanje;
-                this.frekvenca = this.frekvence[oktava, Array.IndexOf(this.toni, ton)];
-            }
-
-            public double getTrajanje()
-            {
-                return this.trajanje;
-            }
-
-            public double getFrekvenca()
-            {
-                return this.frekvenca;
-            }
         }
     }
 }
